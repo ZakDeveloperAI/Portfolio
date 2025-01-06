f406e7081f259f25b7d10cf5bd5879f49fff2e8e
ZakDeveloperAI <belfakhirzakaria@gmail.com>
2025-01-06 17:20:14 +0100

    finished footer and fixed some layout issues

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { SiWhatsapp } from 'react-icons/si';
 import { BiPhoneCall } from "react-icons/bi";
 const Footer = () => {
     return (
-        <div className='hidden mdl:inline-flex xl:hidden items-center justify-center w-full 
+        <div className=' inline-flex  items-center justify-center w-full 
         py-6 gap-4'>
             <a href="https://github.com/ZakDeveloperAI" target='_blank'>
                 <span className='w-10 h-10 text-xl  bg-hoverColor rounded-full inline-flex 
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Archive from "@/components/Archive";
 import ContactMe from "@/components/ContactMe";
+import Footer from "@/components/Footer";
 
 
 
@@ -23,7 +24,8 @@ export default function Home() {
       <link rel="icon" href={"/assets/images/ZakCVphoto.png" }/>
     </Head>
       <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight 
-        overflow-x-hidden overflow-y-scroll">
+        overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20
+        scrollbar-thumb-textDark/60">
         <Navbar/>
         <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
           <motion.div
@@ -40,6 +42,7 @@ export default function Home() {
             <Projects/>
             <Archive/>
             <ContactMe/>
+            <Footer/>
           </div>
           <motion.div
             initial={{ opacity: 0 }}
